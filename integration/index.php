27c3af40451d70c916dab13fa4e80c99f3dd9a2c
sebastianbuettrich <sebastian@less.dk>
2019-03-06 16:35:15 +0100

    http integration

--- integration/index.php
+++ integration/index.php
@@ -6,13 +6,14 @@
 $myFile = fopen("../js/nodes.js", "a") ;
 $data1 = file_get_contents('php://input');
 $data1 = serialize($data1);
+/////////// cleaning up
 $pattern = '/^s:[0-9]*:\"/';
 $replacement = 'points.push(';
 $data1 = preg_replace($pattern, $replacement, $data1);
 $pattern = '/\}\";$/';
 $replacement = '});';
 $data1 = preg_replace($pattern, $replacement, $data1);
-echo $data1;
+//// done cleaning up
 fwrite($myFile, $data1."\n");
 fclose($myFile);
 ?>
