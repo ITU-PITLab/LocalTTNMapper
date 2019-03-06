81008ae7056630722d7a5abd02c8b109e97b59c5
sebastianbuettrich <sebastian@less.dk>
2019-03-06 16:33:50 +0100

    created an http integration

---
+++ integration/index.php
@@ -0,0 +1,18 @@
+<?php
+// This is the receiving end of the TTN integration
+// receiving the objects, doing a bit of clean up and writing into ../js/nodes.js
+// It is an alternative to having /home/coverage.py running
+
+$myFile = fopen("../js/nodes.js", "a") ;
+$data1 = file_get_contents('php://input');
+$data1 = serialize($data1);
+$pattern = '/^s:[0-9]*:\"/';
+$replacement = 'points.push(';
+$data1 = preg_replace($pattern, $replacement, $data1);
+$pattern = '/\}\";$/';
+$replacement = '});';
+$data1 = preg_replace($pattern, $replacement, $data1);
+echo $data1;
+fwrite($myFile, $data1."\n");
+fclose($myFile);
+?>
